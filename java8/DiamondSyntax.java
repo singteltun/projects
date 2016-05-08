f1e262179e12750fe3bbd4b4ecc9d7f4fae05669
singteltun <tunlinaung@singtel.com>
2016-05-08 12:04:28 +0800

    java7 features

---
+++ java8/DiamondSyntax.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class DiamondSyntax {
+
+  public static void main(String args[]) {
+    Map<String, Map<String, String>> userLists = new HashMap<String, Map<String, String>>();
+    System.out.println("Old diamond : " + userLists);
+    
+    Map<String, Map<String, String>> newUserLists = new HashMap<>();
+    System.out.println("new diamond : " + newUserLists);
+  }
+
+}
